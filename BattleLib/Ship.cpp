fa44837b122df61ec81816e34f5136e2b9abd355
Khaferl Pavel <khaferpavel@gmail.com>
2026-09-22 17:24:13 +0300

    добавил невероятный конструктор для класса Ship

---
+++ BattleLib/Ship.cpp
@@ -0,0 +1,58 @@
+#include "Ship.h"
+#include <algorithm>
+
+Ship::Ship(std::vector<Position> pos) {
+	size_t size = pos.size();
+	if (size <= 0) {
+		throw std::invalid_argument("zero size");
+	}
+	if (size > 4) {
+		throw std::invalid_argument("invalid size of ship");
+	}
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < i; j++) {
+			if (pos[i] == pos[j]) {
+				throw std::invalid_argument("two equal cells are taken");
+			}
+		}
+	}
+	bool all_x_same = true;
+	bool all_y_same = true;
+	int x0 = pos[0].getx();
+	int y0 = pos[0].gety();
+	for (int i = 1; i < size; i++) {
+		if (pos[i].getx() != x0) {
+			all_x_same = false;
+		}
+		if (pos[i].gety() != y0) {
+			all_y_same = false;
+		}
+	}
+	if (all_x_same == false && all_y_same == false) {
+		throw std::invalid_argument("not a line ship");
+	}
+	std::vector<int> arrx;
+	std::vector<int> arry;
+	for (int i = 0; i < size; i++) {
+		arrx.push_back(pos[i].getx());
+		arry.push_back(pos[i].gety());
+	}
+	std::sort(arrx.begin(), arrx.end());
+	std::sort(arry.begin(), arry.end());
+	if (all_x_same == true) {
+		for (int i = 0; i < size - 1; i++) {
+			if (arry[i + 1] != arry[i] + 1) {
+				throw std::invalid_argument("not a straight line ship");
+			}
+		}
+	}
+	if (all_y_same == true) {
+		for (int i = 0; i < size - 1; i++) {
+			if (arrx[i + 1] != arrx[i] + 1) {
+				throw std::invalid_argument("not a straight line ship");
+			}
+		}
+	}
+	positions = std::move(pos);
+}
+
--- BattleLib/Ship.h
+++ BattleLib/Ship.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "Position.h"
+#include <vector>
+#include <stdexcept>
+
+class Ship {
+private:
+	std::vector<Position> positions;
+	std::vector<Position> hits;
+public:
+	Ship(std::vector<Position> pos);
+	const std::vector<Position>& get_positions() const noexcept;
+	const std::vector<Position>& get_hits() const noexcept;
+	bool operator==(const Ship& other) const;
+	bool contains(const Position& pos) const;
+	bool hit(const Position& pos);
+	bool is_sunk() const;
+
+};
